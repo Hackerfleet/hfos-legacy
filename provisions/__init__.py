f7c456cb6ac5e144b829600784237e13c1875df6
riot <riot@hackerfleet.org>
2015-04-07 21:20:39 +0200

    Initial provisioning package

---
+++ provisions/__init__.py
@@ -0,0 +1,18 @@
+"""
+Hackerfleet Operating System - Backend
+
+Package: Provisions
+===================
+
+Initial client configuration data.
+This contains tilelayer urls, api stuff etc.
+
+:copyright: (C) 2011-2015 riot@hackerfleet.org
+:license: GPLv3 (See LICENSE)
+
+"""
+
+__author__ = "Heiko 'riot' Weinen <riot@hackerfleet.org>"
+
+__all__ = ['layers'
+]
