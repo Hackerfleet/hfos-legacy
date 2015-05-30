c08b3f32bb647fc8b2c5d148696f6bcad82edfd7
riot <riot@hackerfleet.org>
2015-05-30 20:46:32 +0200

    Upped pynmea to 0.7.0

--- setup.py
+++ setup.py
@@ -73,11 +73,11 @@ setup(name="hfos",
       ],
 
       long_description=include_readme(),
-      dependency_links=['https://github.com/Hackerfleet/pynmea/archive/master.zip#egg=Pynmea-0.3.0-hf',
+      dependency_links=['https://github.com/Hackerfleet/pynmea/archive/master.zip#egg=Pynmea-0.7.0',
                         'https://github.com/Hackerfleet/warmongo/archive/master.zip#egg=warmongo-0.5.2-hf'
       ],
       install_requires=['circuits==3.1.0',
-                        'Pynmea==0.3.0-hf',
+                        'Pynmea==0.7.0',
                         'warmongo==0.5.2-hf',
                         'jsonschema==2.4.0'
       ]
