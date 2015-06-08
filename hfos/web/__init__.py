dd70e5a629f35b6e7599231bcdf46943dcf551f1
riot <riot@hackerfleet.org>
2015-06-08 18:25:30 +0200

    Docstring fix

--- hfos/web/__init__.py
+++ hfos/web/__init__.py
@@ -1,1 +1,14 @@
-__author__ = 'riot'
+"""
+Hackerfleet Operating System - Backend - Web bits
+
+Package HFOS.web
+================
+
+The backend package dealing with the web frontend interface.
+
+:copyright: (C) 2011-2015 riot@hackerfleet.org
+:license: GPLv3 (See LICENSE)
+
+"""
+
+__author__ = "Heiko 'riot' Weinen <riot@hackerfleet.org>"
