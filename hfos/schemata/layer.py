0ae1ba90f7c838916c9fdb4c93544e3d504d3223
riot <riot@hackerfleet.org>
2015-06-14 18:13:47 +0200

    That one had really long lines.

--- hfos/schemata/layer.py
+++ hfos/schemata/layer.py
@@ -41,48 +41,66 @@ Layer = {
             'minZoom': {'type': 'number', 'description': 'Minimum zoom number.', 'default': 0},
             'maxZoom': {'type': 'number', 'description': 'Maximum zoom number.', 'default': 18},
             'maxNativeZoom': {'type': 'number',
-                              'description': 'Maximum zoom number the tiles source has available. If it is specified, the tiles on all zoom levels higher than maxNativeZoom will be loaded from maxZoom level and auto-scaled.',
+                              'description': 'Maximum zoom number the tiles source has available. If it is specified,\
+                               the tiles on all zoom levels higher than maxNativeZoom will be loaded from maxZoom level\
+                               and auto-scaled.',
                               'default': 'null'},
             'tileSize': {'type': 'number',
                          'description': 'Tile size (width and height in pixels, assuming tiles are square).',
                          'default': 256},
             'subdomains': {'type': 'string',
-                           'description': 'Subdomains of the tile service. Can be passed in the form of one string (where each letter is a subdomain name) or an array of strings.',
+                           'description': 'Subdomains of the tile service. Can be passed in the form of one string\
+                            (where each letter is a subdomain name) or an array of strings.',
                            'default': '''abc'''},
             'errorTileUrl': {'type': 'string',
                              'description': 'URL to the tile image to show in place of the tile that failed to load.',
                              'default': ''},
             'attribution': {'type': 'string',
-                            'description': 'e.g. "&copy; Openstreetmap" - the string used by the attribution control, describes the layer data.',
+                            'description': 'e.g. "&copy; Openstreetmap" - the string used by the attribution control,\
+                             describes the layer data.',
                             'default': ''},
             'tms': {'type': 'boolean',
                     'description': 'If true, inverses Y axis numbering for tiles (turn this on for TMS services).',
                     'default': False},
             'continuousWorld': {'type': 'boolean',
-                                'description': 'If set to true, the tile coordinates won''t be wrapped by world width (-180 to 180 longitude) or clamped to lie within world height (-90 to 90). Use this if you use Leaflet for maps that don''t reflect the real world (e.g. game, indoor or photo maps).',
+                                'description': 'If set to true, the tile coordinates won''t be wrapped by world width\
+                                 (-180 to 180 longitude) or clamped to lie within world height (-90 to 90). Use this\
+                                 if you use Leaflet for maps that don''t reflect the real world (e.g. game, indoor or\
+                                 photo maps).',
                                 'default': False},
             'noWrap': {'type': 'boolean',
-                       'description': 'If set to true, the tiles just won''t load outside the world width (-180 to 180 longitude) instead of repeating.',
+                       'description': 'If set to true, the tiles just won''t load outside the world width (-180 to\
+                        180 longitude) instead of repeating.',
                        'default': False},
             'zoomOffset': {'type': 'number',
                            'description': 'The zoom number used in tile URLs will be offset with this value.',
                            'default': 0},
             'zoomReverse': {'type': 'boolean',
-                            'description': 'If set to true, the zoom number used in tile URLs will be reversed (maxZoom - zoom instead of zoom)',
+                            'description': 'If set to true, the zoom number used in tile URLs will be reversed\
+                             (maxZoom - zoom instead of zoom)',
                             'default': False},
             'opacity': {'type': 'number', 'description': 'The opacity of the tile layer.', 'default': 1.0},
             'zIndex': {'type': 'number', 'description': 'The explicit zIndex of the tile layer. Not set by default.',
                        'default': 'null'},
             'unloadInvisibleTiles': {'type': 'boolean',
-                                     'description': 'If true, all the tiles that are not visible after panning are removed (for better performance). true by default on mobile WebKit, otherwise false.'},
+                                     'description': 'If true, all the tiles that are not visible after panning are\
+                                      removed (for better performance). true by default on mobile WebKit, otherwise\
+                                      false.'},
             'updateWhenIdle': {'type': 'boolean',
-                               'description': 'If false, new tiles are loaded during panning, otherwise only after it (for better performance). true by default on mobile WebKit, otherwise false.'},
+                               'description': 'If false, new tiles are loaded during panning, otherwise only after it\
+                                (for better performance). true by default on mobile WebKit, otherwise false.'},
             'detectRetina': {'type': 'boolean',
-                             'description': 'If true and user is on a retina display, it will request four tiles of half the specified size and a bigger zoom level in place of one to utilize the high resolution.'},
+                             'description': 'If true and user is on a retina display, it will request four tiles of\
+                              half the specified size and a bigger zoom level in place of one to utilize the\
+                              high resolution.'},
             'reuseTiles': {'type': 'boolean',
-                           'description': 'If true, all the tiles that are not visible after panning are placed in a reuse queue from which they will be fetched when new tiles become visible (as opposed to dynamically creating new ones). This will in theory keep memory usage low and eliminate the need for reserving new memory whenever a new tile is needed.'},
+                           'description': 'If true, all the tiles that are not visible after panning are placed in a\
+                            reuse queue from which they will be fetched when new tiles become visible (as opposed to\
+                            dynamically creating new ones). This will in theory keep memory usage low and eliminate\
+                            the need for reserving new memory whenever a new tile is needed.'},
             'bounds': {'type': 'list',
-                       'description': 'When this option is set, the TileLayer only loads tiles that are in the given geographical bounds.'}
+                       'description': 'When this option is set, the TileLayer only loads tiles that are in the given\
+                        geographical bounds.'}
         }
     }
 }
