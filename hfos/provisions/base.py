04f17f73dc38164ceda8defcbe6ea784f920db75
riot <riot@hackerfleet.org>
2015-07-18 16:27:02 +0200

    Preliminary method to drop old provisions

--- hfos/provisions/base.py
+++ hfos/provisions/base.py
@@ -19,18 +19,33 @@ from jsonschema import ValidationError
 
 from hfos.logger import hfoslog, warn
 
+from pymongo import database
 
-def provisionList(items, dbobject):
+
+def provisionList(items, dbobject, overwrite=False, clear=False):
     """Provisions a list of items according to their schema
     :param items: A list of provisionable items.
     :param dbobject: A warmongo database object
     """
 
+    if clear:
+        col_name = dbobject.collection_name()
+        hfoslog('[PROV] Clearing collection for', col_name, lvl=warn)
+
+        import pymongo
+
+        client = pymongo.MongoClient(host="localhost", port=27017)
+        db = client["hfos"]
+
+        db.drop_collection(col_name)
+
+
+
     for item in items:
         itemname = item['name']
         hfoslog('[PROV] Provisioning: Validating object: ', itemname)
 
-        if dbobject.count({'name': itemname}) > 0:
+        if dbobject.count({'name': itemname}) > 0 and not overwrite:
             hfoslog('[PROV] Provisioning: Not updating item ', item, lvl=warn)
         else:
             newobject = dbobject(item)
