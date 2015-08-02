2e85148ee656a9220bc42f77df850aae3029c1e5
riot <riot@hackerfleet.org>
2015-08-02 20:29:45 +0200

    Wiki provisions in

--- hfos/provisions/__init__.py
+++ hfos/provisions/__init__.py
@@ -16,5 +16,6 @@ __author__ = "Heiko 'riot' Weinen <riot@hackerfleet.org>"
 
 __all__ = ['layers',
            'controllables',
-           'controllers'
+           'controllers',
+           'wiki'
            ]
