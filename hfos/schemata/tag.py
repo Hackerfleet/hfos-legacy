8a9d9c265d445fe8aded9b08ba9aa272f473a04a
riot <riot@hackerfleet.org>
2015-11-29 17:49:43 +0100

    Tags defined

---
+++ hfos/schemata/tag.py
@@ -0,0 +1,62 @@
+"""
+
+Schema: Tag
+============
+
+Contains
+--------
+
+Systemwide Tag definition
+
+See also
+--------
+
+Provisions
+
+:copyright: (C) 2011-2015 riot@hackerfleet.org
+:license: GPLv3 (See LICENSE)
+
+"""
+
+__author__ = "Heiko 'riot' Weinen <riot@hackerfleet.org>"
+
+Tag = {
+    'type': 'object',
+    'id': '#Tag',
+    'name': 'Tag',
+    'properties': {
+        'uuid': {'type': 'string', 'minLength': 36, 'title': 'Unique Tag ID', 'description': 'HIDDEN'},
+        'name': {'type': 'string', 'title': 'Name', 'description': 'Name of Tag'},
+        'creator': {'type': 'string', 'title': 'Creator', 'description': 'Creator of Tag'},
+        'owneruuid': {'type': 'string', 'minLength': 36, 'title': "Owner's Unique ID", 'description': 'HIDDEN'},
+        'color': {'type': 'string', 'format': 'color', 'title': 'Color of tag',
+                  'description': 'Background color of tag'},
+        'notes': {'type': 'string', 'format': 'html', 'title': 'User notes',
+                  'description': 'Descriptive Tag notes'},
+        'references': {
+            'type': 'array',
+            'default': [],
+            'items': {
+                'type': 'object',
+                'properties': {
+                    'schema': {'type': 'string', 'minLength': 1, 'title': 'Schema reference',
+                               'description': 'HIDDEN'},
+                    'uuid': {'type': 'string', 'minLength': 36, 'title': 'Unique ID reference', 'description': 'HIDDEN'}
+                }
+            }
+        }
+    }
+}
+
+TagForm = [
+    'name'
+    'color'
+    'notes',
+    {
+        'type': 'submit',
+        'title': 'Save Tag',
+    }
+]
+
+__form__ = TagForm
+__schema__ = Tag
