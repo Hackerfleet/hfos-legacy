36052f02baaae5f7fb1ad95b11e06fbc120c761a
riot <riot@hackerfleet.org>
2016-07-18 00:25:48 +0200

    Fixed test, for real now

--- tests/test_launcher.py
+++ tests/test_launcher.py
@@ -10,13 +10,20 @@ Test HFOS Launcher
 
 """
 
+from hfos.database import initialize
 from hfos.launcher import Core
 
 __author__ = "Heiko 'riot' Weinen <riot@hackerfleet.org>"
 
+initialize()  # Set up database access for testing once
+
 
 def test_launcher():
-    return True
-    #server = Core(port=8123)
-    #
-    #assert type(server) == Core
+    """Tests if the Core Launcher can be instantiated"""
+
+    # Use a non privileged port for testing, until that part can be removed
+    # from Core
+
+    core = Core(port=8123)
+
+    assert type(core) == Core
