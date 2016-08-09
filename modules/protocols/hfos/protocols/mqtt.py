cc18627287dd66bbec05a536a084a4bcabdc7e02
riot <riot@hackerfleet.org>
2016-08-09 19:30:25 +0200

    Protocols package (WiP) added

---
+++ modules/protocols/hfos/protocols/mqtt.py
@@ -0,0 +1,27 @@
+"""
+
+Module: MQTTGateway
+======================
+
+Doing rather not much except serve as module component entrypoint.
+
+:copyright: (C) 2011-2016 riot@hackerfleet.org
+:license: GPLv3 (See LICENSE)
+
+"""
+
+from hfos.component import ConfigurableComponent
+
+__author__ = "Heiko 'riot' Weinen <riot@hackerfleet.org>"
+
+
+class MQTTGateway(ConfigurableComponent):
+    """
+    Empty hull component to store settings etc.
+    """
+
+    configprops = {}
+
+    def __init__(self, **kwargs):
+        super(MQTTGateway, self).__init__('MQTT', **kwargs)
+        self.log("Started.")
---
+++ modules/protocols/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# HFOS - Hackerfleet Operating System - Protocols Module
+# ======================================================
+# Copyright (C) 2011-2016 riot <riot@hackerfleet.org> and others.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+setup(name="hfos-protocols",
+      version="0.0.1",
+      description="hfos-protocols",
+
+      author="Hackerfleet Community",
+      author_email="packages@hackerfleet.org",
+      url="https://github.com/hackerfleet/hfos-protocols",
+      license="GNU General Public License v3",
+      packages=find_packages(),
+      long_description="""HFOS - Protocol Suite
+=====================
+
+A component suite for m2m and other useful communication protocols.
+Currently in development:
+* MQTT
+
+Planned:
+* ZMQ
+* XMPP
+
+This software package is a plugin module for HFOS.
+""",
+      dependency_links=[],
+      install_requires=['hfos==1.1.0'],
+      entry_points="""[hfos.schemata]
+      [hfos.components]
+      mqttgateway=hfos.protocols.mqtt:MQTTGateway
+    """,
+      test_suite="tests.main.main",
+      )
