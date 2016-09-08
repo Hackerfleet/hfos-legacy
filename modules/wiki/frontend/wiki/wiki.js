c23a7920626df0fba1e2e558f47d71a2bc8e87de
riot <riot@hackerfleet.org>
2016-09-08 14:39:28 +0200

    Wiki now works nicely

--- modules/wiki/frontend/wiki/wiki.js
+++ modules/wiki/frontend/wiki/wiki.js
@@ -39,20 +39,31 @@ class wikicomponent {
                 console.log('self.templatelist', self.templatelist);
             }
         });
-        
+
+        this.$scope.$on('OP.Update', function(ev, uuid, obj, schema) {
+            if (schema === "wikipage" && uuid == self.pageuuid) {
+                console.log('[WIKI] Page update received');
+                self.updatePage(obj);
+            }
+        });
+
+        this.updatePage = function(obj) {
+            console.log('[WIKI] Rendering a wikipage!');
+            self.html = obj.html;
+            self.title = obj.title;
+            self.pageuuid = obj.uuid;
+
+            // TODO: Extend this to make it work with external links and handle link titles
+            var brackets = /\[([^\]]+)]/g;
+            self.html = self.html.replace(brackets, '<a href="#/wiki/$1">$1</a>');
+        };
+
         this.$scope.$on('OP.Get', function (ev, uuid, obj, schema) {
             console.log('[WIKI] UUID, OBJ, SCH', uuid, obj, schema);
             if (schema === 'wikipage') {
                 console.log('[WIKI] Got a wikipage: ', obj, 'looking for:', self.pagename);
                 if (obj.name == self.pagename) {
-                    console.log('[WIKI] Got a wikipage!');
-                    console.log('Wikicomponent got a wikipage!');
-                    self.html = obj.html;
-                    self.title = obj.title;
-                    self.pageuuid = obj.uuid;
-                    
-                    var brackets = /\[([^\]]+)]/g;
-                    self.html = self.html.replace(brackets, '<a href="#/wiki/$1">$1</a>');
+                    self.updatePage(obj);
                 } else {
                     console.log('[WIKI] Not our page.');
                 }
@@ -64,13 +75,11 @@ class wikicomponent {
     
     createPage() {
         console.log('Creating empty page with name:', this.pagename, this.selectedtemplate);
-/*        if (this.selectedtemplate == 'Empty') {
-            this.state.go('app.editor', {schema: 'webpage', action: 'create'})
+        if (this.selectedtemplate == 'Empty') {
+            this.state.go('app.editor', {schema: 'wikipage', action: 'create'})
         } else {
             this.alert.add('warning', 'WiP', 'Sorry, this is Work in Progress.', 2);
         }
-        */
-        
     }
     
     editpage() {
