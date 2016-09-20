526655acae40ac31109dcf0b4a7202b9ca57c487
riot <riot@hackerfleet.org>
2016-09-20 15:32:15 +0200

    Adapted to new structure, repaired index generation

--- modules/wiki/hfos/wiki/wiki.py
+++ modules/wiki/hfos/wiki/wiki.py
@@ -17,12 +17,14 @@ from hfos.events.client import send
 from hfos.database import ValidationError, objectmodels
 import pymongo
 
-try:
-    from docutils.core import publish_parts
-except ImportError:
-    publish_parts = None
-    hfoslog("No docutils found! Install it to get full functionality!",
-            lvl=warn, emitter="WIKI")
+
+# TODO: Currently unused:
+# try:
+#     from docutils.core import publish_parts
+# except ImportError:
+#     publish_parts = None
+#     hfoslog("No docutils found! Install it to get full functionality!",
+#             lvl=warn, emitter="WIKI")
 
 __author__ = "Heiko 'riot' Weinen <riot@hackerfleet.org>"
 
@@ -43,128 +45,33 @@ class Wiki(ConfigurableComponent):
 
         self.log("Started")
 
-    def _update_index(self):
-        wikipage = objectmodels['wikipage']
-        index = wikipage.find_one({'name': 'Index'})
-        index.text = "<ul>"
-        for item in wikipage.find(sort=[("name", pymongo.DESCENDING)]):
-            index.text += '<li><a href="#/wiki/' + item.name + '">' + \
-                          item.title + '</a></li>'
-        index.save()
+    def objectcreation(self, event):
+        self._page_update(event)
 
     def objectchange(self, event):
-        """
-        Checks if the newly updated object is a wikipage..
-        If so, renders the new html.
+        self._page_update(event)
 
-        :param event: objectchange event
+    def _page_update(self, event):
+        """
+        Checks if the newly created object is a wikipage..
+        If so, rerenders the automatic index.
+
+        :param event: objectchange or objectcreation event
         """
         try:
             if event.schema == 'wikipage':
-                newpage = objectmodels['wikipage'].find_one(
-                    {'uuid': event.uuid})
-                try:
-                    hfoslog('[WIKI] Got a rendering candidate: ',
-                            newpage._fields)
-
-                    try:
-                        if not publish_parts:
-                            html = newpage.text
-                        else:
-                            html = publish_parts(
-                                newpage.text,
-                                writer_name='html')['html_body']
-
-                        newpage.html = html
-                        newpage.save()
-                        self.log("Page successfully rendered")
-                    except Exception as e:
-                        self.log("Error during html rendering: ", e,
-                                 type(e), newpage.isbn, lvl=error)
-                except Exception as e:
-                    self.log("Error during page update: ", e)
+                self._update_index()
 
         except Exception as e:
-            self.log("Page rendering notification error: ", event, e,
+            self.log("Page creation notification error: ", event, e,
                      type(e), lvl=error)
 
-    def wikirequest(self, event):
-        """Wiki event handler for incoming events
-        :param event: WikiRequest with incoming wiki pagename and pagedata
-        """
-
-        self.log("Event: '%s'" % event.__dict__)
-        try:
-            action = event.action
-            data = event.data
-
-            model = objectmodels['wikipage']
-
-            if action == "get":
-                wikipage = model.find_one({'name': data})
-
-                if not wikipage:
-                    self.log("Page not found: ", data)
-                    wikipage = model({'uuid': str(uuid4()),
-                                      'name': data,
-                                      })
-                else:
-                    self.log("Page found, delivering: ", data)
-
-                wikipacket = {'component': 'wiki',
-                              'action': 'get',
-                              'data': wikipage.serializablefields()
-                              }
-            elif action == "put":
-                if data['uuid']:
-                    wikipage = model.find_one({'uuid': data['uuid']})
-
-                    if wikipage:
-                        self.log("Updating old page.")
-                        wikipage.update(data)
-                    else:
-                        wikipage = model(data)
-                        self.log("Storing a new page:", wikipage._fields)
-                        try:
-                            wikipage.validate()
-                            wikipage.save()
-                        except ValidationError:
-                            self.log("Validation of new page failed!",
-                                     data, lvl=warn)
-
-                        self.log("Page stored. Reindexing.")
-                        self._update_index()
-                        self.log("Reindexing done.")
-
-                    wikipacket = {'component': 'wiki',
-                                  'action': 'put',
-                                  'data': (True, wikipage.name),
-                                  }
-                else:
-                    hfoslog(
-                        "[WIKI] Weird request without uuid! Trying to create "
-                        "completely new page.",
-                        lvl=warn)
-                    wikipage = model(data)
-                    wikipage.uuid = str(uuid4())
-                    if wikipage.validate():
-                        wikipage.save()
-
-                    wikipacket = {'component': 'wiki',
-                                  'action': 'put',
-                                  'data': (True, wikipage.name),
-                                  }
-
-            else:
-                self.log("Unsupported action: ", action, event,
-                         event.__dict__, lvl=warn)
-                return
-
-            try:
-                self.fireEvent(send(event.client.uuid, wikipacket))
-            except Exception as e:
-                self.log("Transmission error before broadcast: %s" % e,
-                         lvl=error)
-
-        except Exception as e:
-            self.log("Error: '%s' %s" % (e, type(e)), lvl=error)
+    def _update_index(self):
+        self.log('Updating page index')
+        wikipage = objectmodels['wikipage']
+        index = wikipage.find_one({'name': 'Index'})
+        index.html = "<ul>"
+        for item in wikipage.find(sort=[("name", pymongo.DESCENDING)]):
+            index.html += '<li><a href="#/wiki/' + item.name + '">' + \
+                          item.title + '</a></li>'
+        index.save()
