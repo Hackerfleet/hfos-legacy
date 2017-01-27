c76d01556c4ef48613a8940502813c87fc534595
riot <riot@c-base.org>
2017-01-27 14:05:12 +0100

    Switched to taskgroups, WiP

--- modules/project/frontend/taskgrid/taskgrid.js
+++ modules/project/frontend/taskgrid/taskgrid.js
@@ -9,36 +9,30 @@
  */
 
 class taskgridcomponent {
-    constructor($scope, user, ObjectProxy, state) {
-
-        ObjectProxy.getList('task', {}, ['name', 'status', 'priority']);
-
+    constructor($scope, $rootScope, user, ObjectProxy, state, menu) {
+        this.scope = $scope;
+        this.rootscope = $rootScope;
         this.user = user;
         this.state = state;
-        this.tasklist = [];
-        this.tasks = {};
         this.op = ObjectProxy;
+        this.menu = menu;
 
         this.columns = {};
         this.columnsize = 4;
-
-        this.columnsVisible = {
-            'Open': 0,
-            'Waiting': 1,
-            'In progress': 2,
-            'Closed': 3,
-            'Resolved': -1,
-            'Duplicate': -1,
-            'Invalid': -1,
-            'Cannot reproduce': -1
-        };
     
+        this.tasklist = [];
+        
+        this.taskgridconfig = {};
+        this.taskgroups = {};
+        this.tasks = {};
+        
         var self = this;
         
         this.updateVisibleColumns = function() {
+            /*
             self.items = [];
             self.populateHeaders();
-
+            
             for (var task of self.tasklist) {
                 console.log(self.columns, task.status);
                 if (self.columnsVisible[task.status] !== -1) {
@@ -54,21 +48,69 @@ class taskgridcomponent {
                     self.columns[task.status].push(newitem);
                 }
             }
+            
 
             self.columnsize = 12 / Object.keys(self.columns).length;
             console.log('COLUMNS:', self.columnsize);
             console.log('Tasks', self.columns);
+            */
         };
-
+        
+        this.switchtaskgridconfig = function (uuid) {
+            self.taskgridconfiguuid = uuid;
+            self.op.getObject('taskgridconfig', uuid);
+        };
+        
+        this.requesttaskgridconfigs = function () {
+            console.log('[TASKGRID] Getting list of taskgridconfigs');
+            self.op.getList('taskgridconfig');
+        };
+               
         this.populateHeaders = function() {
+            /*
             $.each(self.columnsVisible, function (key, value) {
                 if (value !== -1) {
                     self.columns[key] = [];
                 }
             });
             console.log(self.items);
+            */
         };
-
+    
+        this.rootscope.$on('Clientconfig.Update', function () {
+            self.gettaskgridconfig();
+        });
+    
+        if (typeof user.clientconfig.taskgridconfiguuid !== 'undefined') {
+            this.gettaskgridconfig();
+        }
+    
+        this.scope.$on('OP.ListUpdate', function (event, schema) {
+            if (schema === 'taskgridconfig') {
+                var taskgridconfiglist = self.op.lists.taskgridconfig;
+                var taskgridconfigmenu = [];
+                for (var taskgridconfig of taskgridconfiglist) {
+                    taskgridconfigmenu.push({
+                        type: 'func',
+                        name: taskgridconfig.uuid,
+                        text: taskgridconfig.name,
+                        callback: self.switchtaskgridconfig,
+                        args: taskgridconfig.uuid
+                    });
+                }
+                self.menu.addMenu('taskgridconfigs', taskgridconfigmenu);
+            }
+        });
+        
+        this.rootscope.$on('User.Login', function () {
+            console.log('[TASKGRID] Login successful - fetching taskgridconfig data');
+            self.requesttaskgridconfigs();
+        });
+    
+        if (this.user.signedin === true) {
+            console.log('[TASKGRID] Logged in - fetching taskgridconfig data');
+            this.requesttaskgridconfigs();
+        }
         $scope.$on('OP.ListUpdate', function (event, schema) {
             self.tasklist = self.op.lists[schema];
 
@@ -78,7 +120,14 @@ class taskgridcomponent {
 
         this.updateVisibleColumns();
     }
-
+    
+    
+    gettaskgridconfig() {
+        console.log('[TASKGRID] Getting newly configured taskgridconfig');
+        this.taskgridconfiguuid = this.user.clientconfig.taskgridconfiguuid;
+        this.op.getObject('taskgridconfig', this.taskgridconfiguuid);
+    }
+    
     opentab(tabname) {
         console.log('[TASKGRID] Switching tab to ', tabname);
         $('.nav-pills .active, .tab-content .active').removeClass('active');
@@ -101,6 +150,6 @@ class taskgridcomponent {
     }
 }
 
-taskgridcomponent.$inject = ['$scope', 'user', 'objectproxy', '$state'];
+taskgridcomponent.$inject = ['$scope', '$rootScope', 'user', 'objectproxy', '$state', 'menu'];
 
 export default taskgridcomponent;
