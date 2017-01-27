aaeae63a1912fbd651d54dc7d077c0006c358df2
riot <riot@c-base.org>
2017-01-27 14:05:32 +0100

    Switched to taskgroups

---
+++ modules/project/hfos/project/taskgridconfig.py
@@ -0,0 +1,148 @@
+"""
+Schema: Taskgrid
+=================
+
+Contains
+--------
+
+Taskgrid: Taskgrid config to store gridster settings
+
+:copyright: (C) 2011-2016 riot@c-base.org
+:license: GPLv3 (See LICENSE)
+
+"""
+
+from hfos.schemata.defaultform import *
+
+__author__ = "Heiko 'riot' Weinen <riot@c-base.org>"
+
+TaskGridConfigSchema = {
+    'id': '#taskgridconfig',
+    'type': 'object',
+    'name': 'taskgridconfig',
+    'properties': {
+        'uuid': {
+            'pattern': '^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-['
+                       'a-fA-F0-9]{4}-[a-fA-F0-9]{12}$',
+            'type': 'string',
+            'title': 'Unique Taskgrid ID'
+        },
+        'name': {'type': 'string', 'minLength': 1, 'title': 'Name',
+                 'description': 'Taskgrid name'},
+        'locked': {'type': 'boolean', 'title': 'Locked Taskgrid',
+                   'description': 'Determines whether the Taskgrid should '
+                                  'be locked against changes.'},
+        'shared': {'type': 'boolean', 'title': 'Shared Taskgrid',
+                   'description': 'Share Taskgrid with the crew'},
+        'description': {'type': 'string', 'format': 'html',
+                        'title': 'Taskgrid description',
+                        'description': 'Taskgrid description'},
+        'useruuid': {
+            'pattern': '^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-['
+                       'a-fA-F0-9]{4}-[a-fA-F0-9]{12}$',
+            'type': 'string',
+            'title': 'Associated Unique User ID'
+        },
+        'cards': {
+            'type': 'array',
+            'default': [],
+            'items': {
+                'type': 'object',
+                'id': '#Card',
+                'name': 'TaskGridCard',
+                'properties': {
+                    'taskgroup': {
+                        'pattern': '^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-['
+                                   'a-fA-F0-9]{4}-[a-fA-F0-9]{12}$',
+                                   'type': 'string',
+                                           'title': 'Associated Unique Task '
+                                                    'Group ID'
+                    },
+                    'size': {
+                        'type': 'object',
+                        'properties': {
+                            'x': {'type': 'number'},
+                            'y': {'type': 'number'}
+                        }
+                    },
+                    'position': {
+                        'type': 'object',
+                        'properties': {
+                            'width': {'type': 'number'},
+                            'height': {'type': 'number'}
+                        }
+                    },
+                }
+            }
+        }
+    },
+    "required": [
+        'uuid'
+    ]
+}
+
+TaskGridConfigForm = [
+    {
+        'type': 'section',
+        'htmlClass': 'row',
+        'items': [
+            {
+                'type': 'section',
+                'htmlClass': 'col-xs-4',
+                'items': [
+                    'name'
+                ]
+            },
+            {
+                'type': 'section',
+                'htmlClass': 'col-xs-4',
+                'items': [
+                    'shared', 'locked'
+                ]
+            },
+        ]
+    },
+    {
+        'key': 'cards',
+        'add': "Add Task Group",
+        'style': {
+            'add': "btn-success"
+        },
+        'items': [
+            {
+                'key': 'cards[].taskgroup',
+                'type': 'strapselect',
+                'placeholder': 'Select a Task Group',
+                'options': {
+                    "type": "taskgroup",
+                    "asyncCallback": "$ctrl.getFormData",
+                    "map": {'valueProperty': "uuid",
+                            'nameProperty': 'name'}
+                }
+            },
+            {'type': 'section',
+             'htmlClass': 'row',
+             'items': [
+                 {
+                     'type': 'section',
+                     'htmlClass': 'col-xs-4',
+                     'items': [
+                         'cards[].position'
+                     ]
+                 },
+                 {
+                     'type': 'section',
+                     'htmlClass': 'col-xs-4',
+                     'items': [
+                         'cards[].size'
+                     ]
+                 }
+             ]
+             }
+        ]
+    },
+    'description',
+    editbuttons
+]
+
+TaskGridConfig = {'schema': TaskGridConfigSchema, 'form': TaskGridConfigForm}
---
+++ modules/project/hfos/project/taskgroup.py
@@ -0,0 +1,114 @@
+"""
+
+Schema: Task
+============
+
+Contains
+--------
+
+Task reference entry for the todo management
+
+See also
+--------
+
+Provisions
+
+:copyright: (C) 2011-2016 riot@c-base.org
+:license: GPLv3 (See LICENSE)
+
+"""
+
+from hfos.schemata.defaultform import editbuttons
+
+__author__ = "Heiko 'riot' Weinen <riot@c-base.org>"
+
+TaskGroup = {
+    'type': 'object',
+    'id': '#taskgroup',
+    'name': 'taskgroup',
+    'properties': {
+        'uuid': {'type': 'string', 'minLength': 36, 'title': 'Unique Task '
+                                                             'Group ID',
+                 'description': 'HIDDEN'},
+        'name': {'type': 'string', 'title': 'Name',
+                 'description': 'Name of Task Group'},
+        'project': {'type': 'string', 'title': 'Project',
+                    'description': 'Project, the Task Group belongs to'},
+        'creator': {'type': 'string', 'title': 'Creator',
+                    'description': 'Creator of Task Group'},
+        'owneruuid': {'type': 'string', 'minLength': 36,
+                      'title': "Owner's Unique ID", 'description': 'HIDDEN'},
+        'tags': {'type': 'string', 'title': 'Tags',
+                 'description': 'Attached tags'},
+        'color': {'type': 'string', 'title': 'Task Group Color',
+                  'format': 'color',
+                  'description': 'Color used for this Task Group'},
+        'priority': {'type': 'number', 'title': 'Priority',
+                     'description': '1 is Highest priority', 'minimum': 1},
+        'notes': {'type': 'string', 'format': 'html', 'title': 'User notes',
+                  'description': 'Entry notes'},
+    }
+}
+
+TaskGroupForm = [
+    {
+        'type': 'section',
+        'htmlClass': 'row',
+        'items': [
+            {
+                'type': 'section',
+                'htmlClass': 'col-xs-6',
+                'items': [
+                    'name', 'tags', {
+                        'key': 'project',
+                        'type': 'strapselect',
+                        'placeholder': 'Select a Project',
+                        'options': {
+                            "type": "project",
+                            "asyncCallback": "$ctrl.getFormData",
+                            "map": {'valueProperty': "uuid",
+                                    'nameProperty': 'name'}
+                        }
+                    }
+                ]
+            },
+            {
+                'type': 'section',
+                'htmlClass': 'col-xs-6',
+                'items': [
+                    'color', 'priority'
+                ]
+            },
+        ]
+    },
+    'notes',
+    {
+        'type': 'fieldset',
+        'items': [
+            {
+                'key': 'references',
+                'add': 'Add Reference',
+                'style': {
+                    'add': 'btn-success'
+                },
+                'items': [
+                    "references[].referencetype",
+                    {
+                        'key': 'references[].task',
+                        'type': 'strapselect',
+                        'placeholder': 'Select a Project',
+                        'options': {
+                            "type": "task",
+                            "asyncCallback": "$ctrl.getFormData",
+                            "map": {'valueProperty': "uuid",
+                                    'nameProperty': 'name'}
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    editbuttons
+]
+
+TaskGroup = {'schema': TaskGroup, 'form': TaskGroupForm}
