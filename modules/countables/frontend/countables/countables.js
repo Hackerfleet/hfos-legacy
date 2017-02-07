d2c8ef5a1c1fa32450a4ea1cfd6743ca0d910be3
riot <riot@c-base.org>
2017-02-07 19:41:21 +0100

    Fixed countables display

--- modules/countables/frontend/countables/countables.js
+++ modules/countables/frontend/countables/countables.js
@@ -16,29 +16,22 @@ class countablescomponent {
             {word: 'Another', size: 5, id: '80311540-5dc7-4dfa-9374-2c75984c7e26'},
             {word: 'Welt!', size: 3, id: 'ce9bef68-e548-4fa4-be1f-27917eb5a00c'}*/
         ];
-
+        
         this.getCountables = function () {
-            console.log('Getting countables')
-            self.op.getList('countable', {}, ['amount'])
-        };
-
-        this.fillCountables = function (ev, schema) {
-            console.log('Countable LISTUPDATE:', ev, schema);
-            if (schema === 'countable') {
-                var list = self.op.lists['countable'];
+            self.op.searchItems('countable', '', ['amount']).then(function(result){
                 var tags = [];
                 var max = 0;
-                for (var item of list) {
-                    tags.push({word: item.name + '(' + item.amount + ')', size: item.amount, id: item.uuid});
+                for (var item of result.data) {
+                    tags.push({name: item.name + '(' + item.amount + ')', size: item.amount, uuid: item.uuid});
                     max = Math.max(max, item.amount);
                 }
                 for (var item of tags) {
-                    item.size = Math.round((item.size / max) * 10);
+                    item.size = Math.max(8, Math.round((item.size / max) * 42));
                     console.log(item)
                 }
                 self.words = tags;
-            }
-
+            });
+                
             //self.scope.$apply();
         };
 
@@ -50,15 +43,13 @@ class countablescomponent {
         }
 
         $scope.$on('User.Login', this.getCountables);
-        $scope.$on('OP.ListUpdate', this.fillCountables)
-
     }
 
-    count(data) {
+    count(uuid) {
         var packet = {
             component: 'countablewatcher',
             action: 'count',
-            data: data
+            data: uuid
         };
 
         console.log('Sending countable packet:', packet);
