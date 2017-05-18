4129c501c04b75cd7b6ed7d93c23745aa1f6a77b
riot <riot@c-base.org>
2017-05-18 17:14:24 +0200

    Added traceback logging and handler for new event system

--- hfos/component.py
+++ hfos/component.py
@@ -19,9 +19,10 @@ Provisions
 :license: GPLv3 (See LICENSE)
 
 """
-
-from hfos.schemata.component import ComponentBaseConfigSchema, ComponentConfigSchemaTemplate
-from hfos.logger import hfoslog, warn, critical, error, debug, verbose
+from hfos.events.system import authorizedevent
+from hfos.schemata.component import ComponentBaseConfigSchema, \
+    ComponentConfigSchemaTemplate
+from hfos.logger import hfoslog, warn, critical, error, debug, verbose, hilight
 from circuits import Component
 from jsonschema import ValidationError
 from warmongo import model_factory
@@ -30,11 +31,96 @@ from random import randint
 from uuid import uuid4
 from copy import deepcopy
 import inspect
+import traceback
 from sys import exc_info
 
 from pprint import pprint
 
 __author__ = "Heiko 'riot' Weinen <riot@c-base.org>"
+
+
+def handler(*names, **kwargs):
+    """Creates an Event Handler
+
+    This decorator can be applied to methods of classes derived from
+    :class:`circuits.core.components.BaseComponent`. It marks the method as a
+    handler for the events passed as arguments to the ``@handler`` decorator.
+    The events are specified by their name.
+
+    The decorated method's arguments must match the arguments passed to the
+    :class:`circuits.core.events.Event` on creation. Optionally, the
+    method may have an additional first argument named *event*. If declared,
+    the event object that caused the handler to be invoked is assigned to it.
+
+    By default, the handler is invoked by the component's root
+    :class:`~.manager.Manager` for events that are propagated on the channel
+    determined by the BaseComponent's *channel* attribute.
+    This may be overridden by specifying a different channel as a keyword
+    parameter of the decorator (``channel=...``).
+
+    Keyword argument ``priority`` influences the order in which handlers
+    for a specific event are invoked. The higher the priority, the earlier
+    the handler is executed.
+
+    If you want to override a handler defined in a base class of your
+    component, you must specify ``override=True``, else your method becomes
+    an additional handler for the event.
+
+    **Return value**
+
+    Normally, the results returned by the handlers for an event are simply
+    collected in the :class:`circuits.core.events.Event`'s :attr:`value`
+    attribute. As a special case, a handler may return a
+    :class:`types.GeneratorType`. This signals to the dispatcher that the
+    handler isn't ready to deliver a result yet.
+    Rather, it has interrupted it's execution with a ``yield None``
+    statement, thus preserving its current execution state.
+
+    The dispatcher saves the returned generator object as a task.
+    All tasks are reexamined (i.e. their :meth:`next()` method is invoked)
+    when the pending events have been executed.
+
+    This feature avoids an unnecessarily complicated chaining of event
+    handlers. Imagine a handler A that needs the results from firing an
+    event E in order to complete. Then without this feature, the final
+    action of A would be to fire event E, and another handler for
+    an event ``SuccessE`` would be required to complete handler A's
+    operation, now having the result from invoking E available
+    (actually it's even a bit more complicated).
+
+    Using this "suspend" feature, the handler simply fires event E and
+    then yields ``None`` until e.g. it finds a result in E's :attr:`value`
+    attribute. For the simplest scenario, there even is a utility
+    method :meth:`circuits.core.manager.Manager.callEvent` that combines
+    firing and waiting.
+    """
+
+    def wrapper(f):
+        if names and isinstance(names[0], bool) and not names[0]:
+            f.handler = False
+            return f
+
+        if len(names) > 0 and inspect.isclass(names[0]) and issubclass(names[\
+                0], authorizedevent):
+            f.names = (str(names[0].realname()),)
+        else:
+            f.names = names
+
+        f.handler = True
+
+        f.priority = kwargs.get("priority", 0)
+        f.channel = kwargs.get("channel", None)
+        f.override = kwargs.get("override", False)
+
+        args = inspect.getargspec(f)[0]
+
+        if args and args[0] == "self":
+            del args[0]
+        f.event = getattr(f, "event", bool(args and args[0] == "event"))
+
+        return f
+
+    return wrapper
 
 
 class ConfigurableComponent(Component):
@@ -90,12 +176,18 @@ class ConfigurableComponent(Component):
             except ValidationError as e:
                 self.log("Error during configuration reading: ", e, type(e),
                          exc=True)
+        #
+        # for func in inspect.getmembers(self, predicate=inspect.ismethod):
+        #
+        #     if func[0].startswith('request_'):
+        #         self.log('REQUEST FOUND. ADDING TO AUTHORIZED EVENTS',
+        #                  lvl=hilight)
 
     def register(self, *args):
         super(ConfigurableComponent, self).register(*args)
         from hfos.database import configschemastore
-        #self.log('ADDING SCHEMA:')
-        #pprint(self.configschema)
+        # self.log('ADDING SCHEMA:')
+        # pprint(self.configschema)
         configschemastore[self.name] = self.configschema
 
     def unregister(self):
@@ -110,7 +202,7 @@ class ConfigurableComponent(Component):
             self.log("No database access! Check if mongodb is running "
                      "correctly.", lvl=critical)
         if self.config:
-            self.log("Configuration read.", lvl=debug)
+            self.log("Configuration read.", lvl=verbose)
         else:
             self.log("No configuration found.", lvl=warn)
             # self.log(self.config)
@@ -194,7 +286,8 @@ class ConfigurableComponent(Component):
         if 'exc' in kwargs and kwargs['exc'] is True:
             exc_type, exc_obj, exc_tb = exc_info()
             line_no = exc_tb.tb_lineno
-            print('EXCEPTION DATA:', line_no, exc_type, exc_obj, exc_tb)
+            #print('EXCEPTION DATA:', line_no, exc_type, exc_obj, exc_tb)
+            args += traceback.extract_tb(exc_tb),
         else:
             line_no = func.co_firstlineno
 
