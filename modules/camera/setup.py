1d2819aecfbf9867e87565d54b3cd80c62705e94
riot <riot@c-base.org>
2017-05-25 16:03:58 +0200

    Adapted to new event names

--- modules/camera/setup.py
+++ modules/camera/setup.py
@@ -39,7 +39,7 @@ This software package is a plugin module for HFOS.
       dependency_links=[],
       install_requires=['hfos>=1.1.0'],
       entry_points="""[hfos.components]
-    camera=hfos.camera.manager:CameraManager
+    camera=hfos.camera.manager:Manager
 [hfos.schemata]
     """,
       test_suite="tests.main.main",
