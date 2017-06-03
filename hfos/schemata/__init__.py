6858bbc39af1d8d7c96b3a9be979f4a927cf186a
riot <riot@c-base.org>
2017-06-03 18:32:33 +0200

    Copyright fixed

--- hfos/schemata/__init__.py
+++ hfos/schemata/__init__.py
@@ -12,28 +12,7 @@ Contains
 profile.py: User profile objects
 user.py: User account objects
 
-:copyright: (C) 2011-2016 riot@c-base.org
-:license: GPLv3 (See LICENSE)
-
 """
 
 __author__ = "Heiko 'riot' Weinen <riot@c-base.org>"
-
-__license__ = """
-Hackerfleet Technology Demonstrator
-=====================================================================
-Copyright (C) 2011-2016 riot <riot@c-base.org> and others.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+__license__ = "GPLv3"
