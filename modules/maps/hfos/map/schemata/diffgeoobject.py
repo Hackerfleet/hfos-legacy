c386e59ebac71bbe55a7a1ab5d19c0f78c70e70a
riot <riot@c-base.org>
2017-08-15 17:16:56 +0200

    Schemata moved, 1st migrations package created

---
+++ modules/maps/hfos/map/schemata/diffgeoobject.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# HFOS - Hackerfleet Operating System
+# ===================================
+# Copyright (C) 2011-2017 Heiko 'riot' Weinen <riot@c-base.org> and others.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "Heiko 'riot' Weinen"
+__license__ = "GPLv3"
+
+"""
+
+Schema: Geoobject
+=================
+
+Contains
+--------
+
+Geoobject: General geoobject configuration data encapsulating geojson
+
+See also
+--------
+
+Provisions
+
+
+"""
+
+from hfos.schemata.defaultform import defaultform
+from hfos.schemata.base import base_object
+
+GeoObjectSchema = base_object('geoobject', all_roles='crew')
+
+GeoObjectSchema['properties'].update({
+    'layer': {
+        'pattern': '^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-['
+                   'a-fA-F0-9]{4}-[a-fA-F0-9]{12}$',
+        'type': 'string',
+        'title': 'Unique GeoObject Layer ID'
+    },
+    'iconcolor': {'type': 'string', 'title': 'Icon Color',
+                  # 'format': 'color',
+                  'description': 'GeoObject icon color indicator', 'default':
+                      'white'},
+    'newvar': {'type': 'string', 'title': 'Foobar'},
+    'icon': {'type': 'number', 'title': 'Icon',
+             'description': 'Custom user icon', 'default': 'flag'},
+    'shared': {'type': 'boolean', 'title': 'Shared GeoObject',
+               'description': 'Share GeoObject with the crew'},
+    'notes': {'type': 'string', 'format': 'html', 'title': 'User notes',
+              'description': 'Custom user notes'},
+    'minZoom': {'type': 'number', 'description': 'Minimum zoom number.',
+                'default': 0},
+    'maxZoom': {'type': 'number', 'description': 'Maximum zoom number.',
+                'default': 18},
+    'opacity': {'type': 'number',
+                'description': 'The opacity of the GeoObject.',
+                'default': 1.0},
+    'zIndexFoo': {'type': 'number',
+                  'description': 'The explicit zIndex of the GeoObject.'
+                                 'Not set by default.',
+                  },
+
+    'geojson': {
+        'type': 'object',
+        'default': {},
+        'properties': {
+            'newsub': {'type': 'string'}
+        }
+    }
+}
+)
+GeoObject = {'schema': GeoObjectSchema, 'form': defaultform}
---
+++ modules/maps/hfos/map/schemata/mapview.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# HFOS - Hackerfleet Operating System
+# ===================================
+# Copyright (C) 2011-2017 Heiko 'riot' Weinen <riot@c-base.org> and others.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "Heiko 'riot' Weinen"
+__license__ = "GPLv3"
+
+"""
+
+Schema: Mapview
+===============
+
+Contains
+--------
+
+MapView: User generated Mapviews
+
+
+"""
+from hfos.schemata.defaultform import lookup_field, editbuttons
+from hfos.schemata.base import base_object
+
+MapViewSchema = base_object('mapview',
+                            roles_read=['crew'],
+                            roles_write=['navigator', 'crew'],
+                            roles_create=['navigator', 'crew'],
+                            roles_list=['crew']
+                            )
+
+MapViewSchema['properties'].update({
+    'name': {'type': 'string', 'minLength': 1, 'title': 'Name',
+             'description': 'Name of view'},
+    'color': {'type': 'string', 'title': 'View Color', 'format': 'color',
+              'description': 'This views color indicator'},
+    'notes': {'type': 'string', 'format': 'html', 'title': 'User notes',
+              'description': 'Custom user notes'},
+    'viewtype': {'enum': ['vessel', 'user'], 'title': 'View Type',
+                 'description': 'Shows what type of mapview this is'},
+    'layergroups': {
+        'type': 'array',
+        'description': 'List of available Layergroups',
+        'items': {
+            'pattern': '^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-['
+                       'a-fA-F0-9]{4}-[a-fA-F0-9]{12}$',
+            'type': 'string',
+            'title': 'Layergroup',
+            'description': 'Select a Layergroup'
+        },
+        'default': []
+    },
+    'coords': {
+        "lat": {
+            "id": "lat",
+            "type": "number",
+            "maximum": 90,
+            "minimum": -90,
+            "title": "Latitude of coordinate.",
+            "description": "",
+            "name": "lat",
+            "default": 54.17805
+        },
+        "lng": {
+            "id": "lng",
+            "type": "number",
+            "maximum": -180,
+            "minimum": 180,
+            "title": "Longitude of coordinate.",
+            "description": "",
+            "name": "lon",
+            "default": 7.88669
+        },
+        "zoom": {
+            "id": "zoom",
+            "type": "integer",
+            "multipleOf": 1,
+            "maximum": 20,
+            "minimum": 1,
+            "title": "Zoom of view.",
+            "description": "",
+            "name": "zoom",
+            "default": 16
+        },
+        "autoDiscover": {
+            "id": "autoDiscover",
+            "type": "boolean",
+            "title": "Autodiscover on device.",
+            "description": "",
+            "name": "autoDiscover",
+            "default": True
+        }
+
+    }
+})
+
+MapViewForm = [
+    'name',
+    'color',
+    'notes',
+    {
+        'key': 'layergroups',
+        'validationMessage': ' ',
+        'disableErrorState': True,
+        'items': [
+            lookup_field('layergroups[]', 'layergroup'),
+        ]
+    },
+    editbuttons
+]
+
+MapView = {'schema': MapViewSchema, 'form': MapViewForm}
