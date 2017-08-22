9ec3db020737c860b1bcae12ae10b5216328e5be
riot <riot@c-base.org>
2017-08-22 19:45:09 +0200

    Moved template tools to hfos.tools

--- dev/templates/__init__.py
+++ dev/templates/__init__.py
@@ -2,33 +2,3 @@
 #from pprint import pprint
 
 
-def format_template(template, content):
-    import pystache
-    result = u""
-    if True:  # try:
-        result = pystache.render(template, content, string_encoding='utf-8')
-    # except (ValueError, KeyError) as e:
-    #    print("Templating error: %s %s" % (e, type(e)))
-
-    # pprint(result)
-    return result
-
-
-def format_template_file(filename, content):
-    with open(filename, 'r') as f:
-        template = f.read()
-        if type(template) != str:
-            template = template.decode('utf-8')
-
-    return format_template(template, content)
-
-
-def write_template_file(source, target, content):
-    # print(formatTemplateFile(source, content))
-    print(target)
-    data = format_template_file(source, content)
-    with open(target, 'w') as f:
-        for line in data:
-            if type(line) != str:
-                line = line.encode('utf-8')
-            f.write(line)
