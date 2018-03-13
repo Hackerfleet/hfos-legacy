e567bfa2811135126dbef0cf100cde0a0ff499e2
ri0t <riot@c-base.org>
2018-03-13 21:38:41 +0100

    Manual version reintroduced

---
+++ hfos/version.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# HFOS - Hackerfleet Operating System
+# ===================================
+# Copyright (C) 2011-2018 Heiko 'riot' Weinen <riot@c-base.org> and others.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "Heiko 'riot' Weinen"
+__license__ = "AGPLv3"
+
+"""Version Module
+
+Unified HFOS wide version number.
+"""
+
+version_info = (1, 2, 0, "dev")  # (major, minor, patch, dev?)
+version = (
+    ".".join(map(str, version_info))
+    if version_info[-1] != "dev"
+    else "dev"
+)
