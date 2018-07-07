30c4a1644ce1414988c922ee5ba796935f45f678
ri0t <riot@c-base.org>
2018-07-07 19:41:40 +0200

    Do not build a provisionstore by import

--- hfos/provisions/__init__.py
+++ hfos/provisions/__init__.py
@@ -12,33 +12,30 @@ This contains tilelayer urls, api stuff etc.
 
 """
 
-from hfos.logger import hfoslog, debug  # , verbose, error, warn
+from hfos.logger import hfoslog, debug, warn  # , verbose, error, warn
 from pkg_resources import iter_entry_points
 
 __author__ = "Heiko 'riot' Weinen <riot@c-base.org>"
 
 
 # TODO: This probably has to be moved somewhere else (due to clashes with namespace-architecture)
-def _build_provisionstore():
+def build_provision_store():
     available = {}
 
     for provision_entrypoint in iter_entry_points(group='hfos.provisions',
                                                   name=None):
         hfoslog("Provisions found: ", provision_entrypoint.name, lvl=debug,
                 emitter='DB')
-        # try:
-        available[provision_entrypoint.name] = provision_entrypoint.load()
-        # except ImportError as e:
-        #    hfoslog("Problematic provision: ", e, type(e),
-        #            provision_entrypoint.name, exc=True, lvl=warn,
-        #            emitter='PROVISIONS')
+        try:
+            available[provision_entrypoint.name] = provision_entrypoint.load()
+        except ImportError:
+            hfoslog("Problematic provision: ", provision_entrypoint.name, exc=True, lvl=warn,
+                    emitter='PROVISIONS', frame_ref=2)
 
     hfoslog("Found provisions: ", sorted(list(available.keys())), emitter='PROVISIONS')
     # pprint(available)
 
     return available
 
-
-provisionstore = _build_provisionstore()
 
 __import__('pkg_resources').declare_namespace(__name__)
