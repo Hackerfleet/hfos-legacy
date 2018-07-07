5f3a347f5519f0e8032a3df9a379adc3ce043c60
ri0t <riot@c-base.org>
2018-07-07 19:37:02 +0200

    Moved outdated calendar dep to this module

--- modules/shareables/frontend/shareables.module.js
+++ modules/shareables/frontend/shareables.module.js
@@ -6,6 +6,9 @@ import { routing } from './shareables.config.js';
 import shareablescomponent from './shareables/shareables.js';
 import template from './shareables/shareables.tpl.html';
 
+import 'angular-bootstrap-calendar/dist/css/angular-bootstrap-calendar.css';
+import ngcalendar from 'angular-bootstrap-calendar';
+
 export default angular
     .module('main.app.shareables', [uirouter, 'mwl.calendar', 'ngAnimate'])
     .config(routing)
