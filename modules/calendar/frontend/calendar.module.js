43fff994b7292288452af76b52a8fc165cf748d0
ri0t <riot@c-base.org>
2018-07-07 19:43:16 +0200

    Moved dependency to module

--- modules/calendar/frontend/calendar.module.js
+++ modules/calendar/frontend/calendar.module.js
@@ -35,8 +35,12 @@ import template from './calendar/calendar.tpl.html';
 import upcomingcomponent from './calendar/upcoming.js';
 import upcomingtemplate from './calendar/upcoming.tpl.html';
 
+import 'fullcalendar/dist/fullcalendar.min.css';
+require('fullcalendar');
+import 'angular-ui-calendar';
+
 export default angular
-    .module('main.app.calendar', [uirouter, 'mwl.calendar', 'ngAnimate'])
+    .module('main.app.calendar', [uirouter, 'ui.calendar', 'ngAnimate'])
     .config(routing)
     .component('calendar', {controller: calendarcomponent, template: template})
     .component('upcoming', {controller: upcomingcomponent, template: upcomingtemplate})
