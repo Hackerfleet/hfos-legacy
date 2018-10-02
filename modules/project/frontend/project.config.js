01763f440a3e4fae7996a39cd4a19cbaa2d1d183
ri0t <riot@c-base.org>
2018-10-02 22:04:27 +0200

    Sidebar, state persistence and other features added

--- modules/project/frontend/project.config.js
+++ modules/project/frontend/project.config.js
@@ -5,11 +5,15 @@ import todoicon from './assets/iconmonstr-clipboard-6.svg';
 export function routing($stateProvider) {
 
     $stateProvider
-        .state('app.taskgrid', {
+        .state('app.taskgrid-non-url', {
             url: '/taskgrid',
             template: '<taskgrid></taskgrid>',
             label: 'Taskgrid',
             icon: taskgridicon
+        })
+        .state('app.taskgrid', {
+            url: '/taskgrid/{taskgrid}/{projects}/{tags}',
+            template: '<taskgrid></taskgrid>',
         })
         .state('app.todo', {
             url: '/todo',
--- modules/project/frontend/taskgrid/taskgrid.js
+++ modules/project/frontend/taskgrid/taskgrid.js
@@ -11,7 +11,8 @@
 import deletionWarning from './modals/deletionWarning.tpl.html'
 
 class taskgridcomponent {
-    constructor($scope, $rootScope, $timeout, user, ObjectProxy, state, menu, notification, modal) {
+    constructor($scope, $rootScope, $timeout, user, ObjectProxy, state, menu,
+                notification, modal, NgTableParams, $stateParams) {
         this.scope = $scope;
         this.rootscope = $rootScope;
         this.timeout = $timeout;
@@ -21,6 +22,9 @@ class taskgridcomponent {
         this.menu = menu;
         this.notification = notification;
         this.modal = modal;
+        $scope.stateParams = $stateParams;
+
+        this.config = {};
 
         this.columns = {};
         this.columnsize = 4;
@@ -32,11 +36,14 @@ class taskgridcomponent {
         this.taskgroups = {};
         this.tasksByGroup = {};
 
-        this.tags = {};
+        this.taskgrids = [];
         this.projects = {};
+        this.tags = {};
 
         this.filter_tag = "";
         this.filter_project = "";
+
+        this.selected_task = null;
 
         this.show_filters = false;
         this.show_filters_tag = false;
@@ -45,19 +52,25 @@ class taskgridcomponent {
         this.selected = [];
         this.selected_groups = [];
 
-        this.target_group = null;
+        this.me_target_group = null;
+        this.me_tag = null;
+        this.me_project = null;
+        this.me_priority = null;
 
         this.search_string = '';
 
         this.changetimeout = null;
         this.lockState = false;
+        this.side_bar_open = false;
 
         this.gridsterOptions = {
             // any options that you can set for angular-gridster (see:  http://manifestwebdesign.github.io/angular-gridster/)
             columns: screen.width / 100,
             rowHeight: 100,
             colWidth: 100,
-            mobileBreakPoint: 200,
+            // mobileBreakPoint: 200,
+            mobileModeEnabled: false, // whether or not to toggle mobile mode when screen width is less than mobileBreakPoint
+
             draggable: {
                 enabled: false
             },
@@ -68,9 +81,71 @@ class taskgridcomponent {
 
         let self = this;
 
+        this.tableParams = new NgTableParams({}, {
+            getData: function (params) {
+                // ajax request to api
+                let filter = params._params.filter;
+
+                if (typeof filter === 'undefined') {
+                    filter = {'owner': self.user.useruuid};
+                }
+
+                if (filter.hasOwnProperty('name')) {
+                    filter = {'name': {'$regex': ".*" + filter.name + ".*"}}
+                }
+
+                console.log("TASK_TABLE1:", params);
+                let limit = params._params.count;
+                let skip = limit * (params._params.page - 1);
+
+                return self.op.search('task', filter, '*', null, false, limit, skip).then(function (msg) {
+                    let tasks = msg.data.list;
+                    params.total(msg.data.size); // recal. page nav controls
+                    console.log("TASK_TABLE2:", params);
+
+                    return tasks;
+                });
+            }
+        });
+
+        this.select_task = function (uuid) {
+            this.selected_task = uuid;
+            this.scope.$broadcast('Changed.UUID', {eid: 'taskEditor', uuid: self.selected_task});
+            if (!this.side_bar_open) {
+                this.open_sidebar();
+            }
+        };
+
+        this.open_sidebar = function () {
+            this.scope.$broadcast('Resize', '75%');
+            this.side_bar_open = true;
+        };
+
+        this.close_sidebar = function () {
+            this.scope.$broadcast('Resize', '99%');
+            this.side_bar_open = false;
+        };
+
         this.switchtaskgridconfig = function (uuid) {
+            console.log('[TASKGRID] Switching taskgrid:', uuid);
             self.taskgridconfiguuid = uuid;
-            self.op.getObject('taskgridconfig', uuid);
+
+            self.tasklist = [];
+            self.tasksByGroup = {};
+
+            self.gettaskgridconfig();
+            /*
+            self.op.get('taskgridconfig', uuid).then(function(msg) {
+                if (msg.action !== 'fail') { */
+                    self.state.go('app.taskgrid', {taskgrid: uuid}, {
+                        location: 'replace',
+                        inherit: false,
+                        notify: false
+                    });/*
+                } else {
+                    console.log('[TASKGRID] Could not get taskgrid:', msg);
+                }
+            });*/
         };
 
         this.storeTaskGridConfig = function () {
@@ -79,7 +154,9 @@ class taskgridcomponent {
             for (let card of self.taskgridconfig.cards) {
                 delete card['$$hashKey'];
             }
-            self.op.putObject('taskgridconfig', self.taskgridconfig);
+            self.op.put('taskgridconfig', self.taskgridconfig).then(function (msg) {
+                console.log('[TASKGRID] Taskgrid stored');
+            });
 
             self.changetimeout = null;
         };
@@ -97,15 +174,11 @@ class taskgridcomponent {
 
         this.gridChangeWatcher = null;
 
-        this.rootscope.$on('Clientconfig.Update', function () {
-            self.gettaskgridconfig();
-        });
-
         if (typeof user.clientconfig.taskgridconfiguuid !== 'undefined') {
             this.gettaskgridconfig();
         }
 
-        this.scope.$on('OP.Get', function (event, uuid, object, schema) {
+        /*this.scope.$on('OP.Get', function (event, uuid, object, schema) {
             if (uuid === self.taskgridconfiguuid) {
                 self.taskgridconfig = object;
 
@@ -128,22 +201,70 @@ class taskgridcomponent {
                 });
             }
         });
+        */
+    }
 
-        this.scope.$on('OP.ListUpdate', function (event, schema) {
-            if (schema === 'taskgridconfig') {
-                self.taskgrids = self.op.lists.taskgridconfig;
-            }
-        });
+    $onInit() {
+        let self = this;
+        let taskgrid = null;
 
-        this.rootscope.$on('User.Login', function () {
-            console.log('[TASKGRID] Login successful - fetching taskgridconfig data');
-            self.gettaskgridconfig();
-        });
+
+        console.log('[TASKGRID] STATEPARAMS: ', this.state.params);
+        console.log('[TASKGRID] This:', this);
+        console.log('[TASKGRID] Scope:', this.scope);
+
+        //console.log('[TASKGRID] Stateparam:', this.scope.state.params.taskgrid);
+
+        if (typeof this.state.params.taskgrid !== 'undefined') {
+            console.log('[TASKGRID] Getting params from stateparams');
+            taskgrid = this.state.params.taskgrid;
+
+            /*
+                projects: this.scope.stateParams.projects,
+                tags: this.scope.stateParams.tags
+            };*/
+            console.log('[TASKGRID] Resulting config:', taskgrid);
+
+        } else {
+            console.log("[TASKGRID] WIP! Getting params from scope:", this.scope.initial, this.initial);
+            /* Work in progress!
+            this.config = {
+                taskgrid: this.project.taskgrid,
+                projects: this.scope.stateParams.projects,
+                tags: this.scope.stateParams.tags
+            };
+
+            console.debug('[TASKGRID] Configuration: ', this.config);
+            */
+        }
+
+        if (taskgrid !== null) {
+            this.taskgridconfiguuid = taskgrid;
+        } else {
+            this.taskgridconfiguuid = this.user.getModuleDefault('taskgriduuid');
+        }
 
         if (this.user.signedin === true) {
             console.log('[TASKGRID] Logged in - fetching taskgridconfig data');
             this.gettaskgridconfig();
         }
+
+        let login_watcher = this.rootscope.$on('User.Login', function () {
+            console.log('[TASKGRID] Login successful - fetching taskgridconfig data');
+            self.gettaskgridconfig();
+        });
+
+        let client_watcher = this.rootscope.$on('Clientconfig.Update', function () {
+            self.gettaskgridconfig();
+        });
+
+        this.scope.$on('$destroy', function () {
+            console.log('[TASKGRID] Destroying');
+            login_watcher();
+            client_watcher();
+        });
+
+
     }
 
     toggleLock() {
@@ -159,9 +280,10 @@ class taskgridcomponent {
     }
 
     gettaskgridconfig() {
-        console.log('[TASKGRID] Getting list of taskgridconfigs');
-        this.op.getList('taskgridconfig');
         let self = this;
+
+        console.log('[TASKGRID] Getting data');
+
         this.op.search('project').then(function (msg) {
             console.log('[TASKGRID] Projects received: ', msg);
             let projects = msg.data.list;
@@ -178,30 +300,46 @@ class taskgridcomponent {
             }
         });
 
-        console.log('[TASKGRID] Getting configured taskgridconfig');
-        /*
-        // TODO: Pull module default configurations from a service
-        let uuid = self.user.clientconfig.modules.taskgriduuid;
-
-        console.log('[TASKGRID] Clientconfig Taskgrid UUID: ', self.taskgriduuid);
-
-        if (uuid === '' || typeof uuid === 'undefined') {
-            console.log('[TASKGRID] Picking user profile taskgrid', self.user.profile);
-            uuid = self.user.profile.modules.taskgriduuid;
-        }
-        if (uuid === '' || typeof uuid === 'undefined') {
-            console.log('[TASKGRID] Picking system profile taskgrid', self.systemconfig.config);
-            uuid = self.systemconfig.config.modules.taskgriduuid;
-        }
-        console.log('[TASKGRID] Final Taskgrid UUID: ', uuid);
-
-        this.taskgridconfiguuid = uuid;
-        */
-
-        this.taskgridconfiguuid = this.user.getModuleDefault('taskgriduuid');
+        this.op.search('taskgridconfig', '', '*').then(function(msg) {
+            console.log('[TASKGRID] Grids received: ', msg);
+            self.taskgrids = msg.data.list;
+        });
 
         console.log('[TASKGRID] Config: ', this.taskgridconfiguuid);
-        this.op.getObject('taskgridconfig', this.taskgridconfiguuid);
+        this.op.get('taskgridconfig', this.taskgridconfiguuid).then(function(msg) {
+            self.taskgridconfig = msg.data.object;
+            console.log('[TASKGRID] Grid: ', self.taskgridconfig);
+
+            console.log('[TASKGRID] Getting groups');
+
+            let taskgroups = [];
+            for (let card of self.taskgridconfig.cards) {
+                taskgroups.push(card.taskgroup);
+            }
+
+            self.op.search('taskgroup', {uuid: {'$in': taskgroups}}, '*').then(function(msg) {
+                for (let group of msg.data.list) {
+                    self.taskgroups[group.uuid] = group;
+                }
+            });
+
+            console.log('[TASKGRID] Getting tasks');
+
+            self.op.search('task', {taskgroup: {'$in': taskgroups}}, '*').then(function (msg) {
+                let tasks = msg.data.list;
+
+                for (let task of tasks) {
+                    let visualTask = task;
+                    visualTask.showDescription = false;
+                    if (typeof self.tasksByGroup[task.taskgroup] === 'undefined') {
+                        self.tasksByGroup[task.taskgroup] = [];
+                    }
+                    self.tasksByGroup[task.taskgroup].push(visualTask);
+                    self.tasklist[task.uuid] = task;
+                }
+            });
+
+        });
     }
 
     new_task(group_uuid) {
@@ -214,18 +352,25 @@ class taskgridcomponent {
         for (let uuid of Object.keys(this.tasklist)) {
             let task = this.tasklist[uuid];
 
-            let name = task.name.toLowerCase(),
-                search = this.search_string.toLowerCase(),
-                description = "";
-            if (typeof task.description !== 'undefined') description = task.description.toLowerCase();
+            if (typeof this.search_string !== 'undefined' && this.search_string !== '') {
 
-            console.log('[TASKGRID] Filters:', name, description, search);
+                let search = this.search_string.toLowerCase(),
+                    name = "",
+                    description = "";
 
-            if ((name.indexOf(search) > -1 || description.indexOf(search) > -1) &&
-                (this.show_filters_project ? this.filter_project === task.project : true) &&
-                ((this.show_filters_tag ? task.tags.indexOf(this.filter_tag) > -1 : true) ||
-                    (this.show_filters_tag && this.filter_tag === '' && task.tags.length === 0))
-            ) {
+                if (typeof task.name !== 'undefined') name = task.name.toLowerCase();
+                if (typeof task.description !== 'undefined') description = task.description.toLowerCase();
+
+                console.log('[TASKGRID] Filters:', name, description, search);
+
+                if ((name.indexOf(search) > -1 || description.indexOf(search) > -1) &&
+                    (this.show_filters_project ? this.filter_project === task.project : true) &&
+                    ((this.show_filters_tag ? task.tags.indexOf(this.filter_tag) > -1 : true) ||
+                        (this.show_filters_tag && this.filter_tag === '' && task.tags.length === 0))
+                ) {
+                    this.selected.push(task.uuid);
+                }
+            } else {
                 this.selected.push(task.uuid);
             }
         }
@@ -249,7 +394,7 @@ class taskgridcomponent {
     }
 
     add_tags() {
-        if (typeof this.tag === 'undefined' || this.tag === null || this.tag === '') {
+        if (typeof this.me_tag === 'undefined' || this.me_tag === null || this.me_tag === '') {
             this.notification.add('warning', 'Select a tag first', 'You have to select a tag to add.', 3);
             return
         }
@@ -258,17 +403,17 @@ class taskgridcomponent {
             let task = this.tasklist[uuid];
             console.log(task);
 
-            if (task.tags.indexOf(this.tag) < 0) {
+            if (task.tags.indexOf(this.me_tag) < 0) {
                 console.log('[TASKGRID] Adding tag.');
-                task.tags.push(this.tag);
+                task.tags.push(this.me_tag);
 
-                this.op.changeObject('task', uuid, {field: 'tags', 'value': task.tags});
+                this.op.changeObject('task', uuid, {field: 'tags', value: task.tags});
             }
         }
     }
 
     remove_tags() {
-        if (typeof this.tag === 'undefined' || this.tag === null || this.tag === '') {
+        if (typeof this.me_tag === 'undefined' || this.me_tag === null || this.me_tag === '') {
             this.notification.add('warning', 'Select a tag first', 'You have to select a tag to be removed.', 3);
             return
         }
@@ -276,10 +421,41 @@ class taskgridcomponent {
         for (let uuid of this.selected) {
             let task = this.tasklist[uuid];
 
-            if (task.tags.indexOf(this.tag) >= 0) {
-                task.tags.splice(task.tags.indexOf(this.tag), 1);
-                this.op.changeObject('task', uuid, {field: 'tags', 'value': task.tags});
+            if (task.tags.indexOf(this.me_tag) >= 0) {
+                task.tags.splice(task.tags.indexOf(this.me_tag), 1);
+                this.op.changeObject('task', uuid, {field: 'tags', value: task.tags});
             }
+        }
+    }
+
+
+    assign_project() {
+        if (typeof this.me_project === 'undefined' || this.me_project === null || this.me_project === '') {
+            this.notification.add('warning', 'Select a project first', 'You have to select a project to assign to', 3);
+            return
+        }
+
+        for (let uuid of this.selected) {
+            let task = this.tasklist[uuid];
+
+            task.project = this.me_project;
+            this.op.changeObject('task', uuid, {field: 'project', value: this.me_project});
+        }
+    }
+
+    remove_project() {
+        for (let uuid of this.selected) {
+            let task = this.tasklist[uuid];
+            task.project = '';
+            this.op.changeObject('task', uuid, {field: 'project', value: ''});
+        }
+    }
+
+    set_priority() {
+        for (let uuid of this.selected) {
+            let task = this.tasklist[uuid];
+            task.priority = this.me_priority;
+            this.op.changeObject('task', uuid, {field: 'priority', value: this.me_priority});
         }
     }
 
@@ -300,7 +476,7 @@ class taskgridcomponent {
 
         for (let uuid of this.selected) {
             console.log('Deleting ticket ', uuid);
-            this.op.deleteObject('task', uuid).then(function(response) {
+            this.op.deleteObject('task', uuid).then(function (response) {
                 if (response.data.uuid === uuid) {
                     let task = self.tasklist[uuid];
                     let group = self.tasksByGroup[task.taskgroup];
@@ -318,7 +494,7 @@ class taskgridcomponent {
 
 
     move() {
-        if (typeof this.target_group === 'undefined' || this.target_group === null || this.target_group === '') {
+        if (typeof this.me_target_group === 'undefined' || this.me_target_group === null || this.me_target_group === '') {
             this.notification.add('warning', 'Select a target group', 'You have to select a target group to mass move tasks.', 3);
             return
         }
@@ -331,18 +507,12 @@ class taskgridcomponent {
 
             old_group.splice(old_group.indexOf(task), 1);
 
-            this.tasksByGroup[this.target_group].push(task);
+            this.tasksByGroup[this.me_target_group].push(task);
 
-            task.taskgroup = this.target_group;
+            task.taskgroup = this.me_target_group;
 
-            this.op.changeObject('task', uuid, {'field': 'taskgroup', 'value': this.target_group});
+            this.op.changeObject('task', uuid, {'field': 'taskgroup', 'value': this.me_target_group});
         }
-    }
-
-    opentab(tabname) {
-        console.log('[TASKGRID] Switching tab to ', tabname);
-        $('.nav-pills .active, .tab-content .active').removeClass('active');
-        $('#' + tabname).addClass('active');
     }
 
     onDropComplete(task, ev, newgroup) {
@@ -363,6 +533,9 @@ class taskgridcomponent {
 
 }
 
-taskgridcomponent.$inject = ['$scope', '$rootScope', '$timeout', 'user', 'objectproxy', '$state', 'menu', 'notification', '$modal'];
+taskgridcomponent.$inject = [
+    '$scope', '$rootScope', '$timeout', 'user', 'objectproxy', '$state', 'menu',
+    'notification', '$modal', 'NgTableParams', '$stateParams'
+];
 
 export default taskgridcomponent;
